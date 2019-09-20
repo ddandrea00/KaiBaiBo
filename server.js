6b9997ecbc9e36075f432b0252b792144326daf8
Drew Dandrea <d.dandrea00@gmail.com>
2019-09-20 19:08:58 -0400

    changed packages to deploy

---
+++ server.js
@@ -0,0 +1,42 @@
+const http = require('http');
+const express = require('express');
+const socketio = require('socket.io');
+
+const KbbGame = require('./kbb-game');
+
+const app = express();
+
+const clientPath = `${__dirname}/../client`;
+console.log(`Serving static from ${clientPath}`);
+
+app.use(express.static(clientPath));
+
+const server = http.createServer(app);
+
+const io = socketio(server);
+
+let waitingPlayer = null;
+
+io.on('connection', (sock) => {
+
+    if (waitingPlayer) {
+        new KbbGame(waitingPlayer, sock);
+        waitingPlayer = null;
+    } else {
+        waitingPlayer = sock;
+        waitingPlayer.emit('message', 'Waiting for opponent to connect...');
+    }
+
+    sock.on('message', (text) => {
+        io.emit('message', text);
+    });
+});
+
+server.on('error', (err) => {
+    console.error('Server error:', err);
+});
+
+server.listen(8080, () => {
+    console.log('Game started on localhost:8080');
+});
+
